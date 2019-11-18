bac521d4bc93c30b807e510077bc71360896766d
Sean Duffy <33455489+SeanD98@users.noreply.github.com>
2019-11-18 21:26:22 +0000

    Fixed Map issues and added runtime checks/requests

--- app/src/main/java/com/example/myrunningapp/Utils/StyledMapActivity.java
+++ app/src/main/java/com/example/myrunningapp/Utils/StyledMapActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.myrunningapp.Activities.FriendsActivity;
@@ -42,16 +44,24 @@ public class StyledMapActivity extends AppCompatActivity implements OnMapReadyCa
     private GoogleMap mMap;
     private LocationRequest mLocationRequest;
 
-    public Location location;
+    public Location mLocation;
     private ArrayList<LatLng> points;
     Polyline line;
     MarkerOptions marker = new MarkerOptions();
+    //LocationManager mLocationManager;
+    LocationManager mLocationManager;
+
+    boolean onFirstLaunch = true;
 
     private static final String TAG = StyledMapActivity.class.getSimpleName();
     private static final String SELECTED_STYLE = "selected_style";
     private static final long INTERVAL = 1000 * 60 * 1; //1 minute
     private static final long FASTEST_INTERVAL = 1000 * 60 * 1; // 1 minute
     private static final float SMALLEST_DISPLACEMENT = 0.25F; //quarter of a meter
+    private static final long LOCATION_REFRESH_TIME = 300;
+    private static final float LOCATION_REFRESH_DISTANCE = 10;
+    public static final int PERMISSIONS_REQUEST_LOCATION = 99;
+    public static final int PERMISSIONS_REQUEST_LOCATION_2 = 99;
 
     private int mSelectedStyleId = R.string.style_label_default;
     private int mStyleIds[] = {
@@ -71,18 +81,27 @@ public class StyledMapActivity extends AppCompatActivity implements OnMapReadyCa
         }
         setContentView(R.layout.activity_maps);
 
-        points = new ArrayList<LatLng>();
+        points = new ArrayList<>();
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if ((checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            && (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+        } else {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_LOCATION_2);
+            }
+        }
+
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-        }
     }
 
     @Override
@@ -97,54 +116,71 @@ public class StyledMapActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            {
-                Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-
-                if (location != null)
-                {
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
-
-                    CameraPosition cameraPosition = new CameraPosition.Builder()
-                            .target(new LatLng(location.getLatitude(), location.getLongitude()))
-                            .zoom(15)
-                            .bearing(90)
-                            .tilt(40)
-                            .build();
-                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-                    marker.position(new LatLng(location.getLatitude(), location.getLongitude()));
-                    marker.title("You are here!");
-                    mMap.addMarker(marker);
-                }
-            }
         setSelectedStyle();
     }
 
+    public void zoomMapToUser(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), 13));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()))
+                .zoom(15)
+                .bearing(90)
+                .tilt(40)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
-    public void onLocationChanged(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude); //you already have this
-
-        points.add(latLng); //added
-
-        redrawLine(); //added
+        marker.position(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()));
+        marker.title("You are here!");
+        mMap.addMarker(marker);
+        onFirstLaunch = false;
     }
+
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            mLocation = location;
+            if (onFirstLaunch)
+            {
+                zoomMapToUser(mMap);
+            } else {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude, longitude); //you already have this
+                points.add(latLng); //added
+                redrawLine();
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     private void redrawLine(){
 
-        mMap.clear();  //clears all Markers and Polylines
+        mMap.clear();
 
         PolylineOptions options = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
         for (int i = 0; i < points.size(); i++) {
             LatLng point = points.get(i);
             options.add(point);
         }
-        mMap.addMarker(marker); //add Marker in current position
-        line = mMap.addPolyline(options); //add Polyline
+        mMap.addMarker(marker);
+        line = mMap.addPolyline(options);
     }
 
     protected void createLocationRequest() {
